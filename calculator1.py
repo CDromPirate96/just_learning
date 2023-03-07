52eb725015b655bc3d62e19c39683e3f1a9072ca
CDromPirate96 <124601875+CDromPirate96@users.noreply.github.com>
2023-03-07 10:26:53 -0800

    March 7th, 2023

---
+++ calculator1.py
@@ -0,0 +1,34 @@
+#Calculator
+
+#Add
+def add(n1, n2):
+    return n1 + n2
+
+#Subtract
+def subtract(n1, n2):
+    return n1 - n2
+
+#Multiply
+def multiply(n1, n2):
+    return n1 * n2
+
+#Divide
+def divide(n1, n2):
+    return n1 / n2
+
+operations = {
+    "+": add,
+    "-": subtract,
+    "*": multiply,
+    "/": divide,
+}
+
+num1 = int(input("What's the first number?: "))
+for symbol in operations:
+    print(symbol)
+operation_symbol = input("Pick an operation from the line abvove: ")
+num2 = int(input("What's the second number?: "))
+calulation_function = operations[operation_symbol]
+answer = calulation_function(num1, num2)
+
+print(f"{num1} {operation_symbol} {num2} = {answer}")
